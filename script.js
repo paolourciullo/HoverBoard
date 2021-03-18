c63cac21ea0a5bc585f1781e1e2ffcae70d78087
Paolo Urciullo <67667160+paolourciullo@users.noreply.github.com>
2021-03-18 14:59:06 -0400

    init commit
    init commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const container = document.getElementById('container');
+const colors = [
+  'red',
+  'green',
+  'blue',
+  'orange',
+  'yellow',
+  'purple',
+  'white',
+  'aliceblue',
+  'goldenrod',
+  'pink',
+  'brown',
+  'aqua',
+  'aquamarine',
+  '#7fff00',
+  '#8b0000',
+  '#FF00FF',
+];
+
+const SQUARES = 10108;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
